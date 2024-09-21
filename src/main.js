2d99de4d0760e104cf743b187aec1a0013277cc6
Deena Alhamel <deenaalhamel@alMacBook-Air-alkhas-bDeena.local>
2024-09-21 16:50:13 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import i18n from './i18n'
 
-createApp(App).mount('#app')
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+createApp(App).use(i18n).use(router).mount('#app')
---
+++ src/views/AboutView.vue
@@ -0,0 +1,7 @@
+
+<template>
+  <div class="container">
+    <h1 class="text-center">Welcome to My BootstrapVue Project</h1>
+    <button class="btn btn-primary">Click Me</button>
+  </div>
+</template>
